d69277ec811c2cc70245849f6e086b40f4b65a56
Jeswin-J <jeswinjoseph164@gmail.com>
2024-09-04 21:10:53 +0530

    App Navigation incomplete

--- GeoAttendance/App.js
+++ GeoAttendance/App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+import Selfie from './src/screens/Selfie';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Guys!! I am Working!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='Home'>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Selfie" component={Selfie} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const Stack = createNativeStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
---
+++ GeoAttendance/src/screens/Selfie.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Selfie = () => {
+  return (
+    <View>
+      <Text>Selfie</Text>
+    </View>
+  )
+}
+
+export default Selfie
