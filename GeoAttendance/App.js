204ecfddebf8b74fbf3b0d3e7cf102f03a0aa5dd
Jeswin-J <jeswinjoseph164@gmail.com>
2024-09-04 23:04:58 +0530

    location permissions incomplete

--- GeoAttendance/App.js
+++ GeoAttendance/App.js
@@ -1,28 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
 import Selfie from './src/screens/Selfie';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='Home'>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Selfie" component={Selfie} />
+        <Stack.Screen name="Details" component={Selfie} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const Stack = createNativeStackNavigator();
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
--- GeoAttendance/src/screens/Selfie.js
+++ GeoAttendance/src/screens/Selfie.js
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 
-const Selfie = () => {
+
+const Selfie = ({navigation}) => {
   return (
-    <View>
-      <Text>Selfie</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   )
 }
