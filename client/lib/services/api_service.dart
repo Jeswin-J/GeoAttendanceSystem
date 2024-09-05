321cf45a1ae03f2b801ecba57588681a96b4fa19
Jeswin-J <jeswinjoseph164@gmail.com>
2024-09-05 20:02:52 +0530

    checkin feature

--- client/lib/services/api_service.dart
+++ client/lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ApiService {
-  static const String apiUrl = "http://localhost:8080/api/attendance/checkin";
+  static const String apiUrl = "http://192.168.29.111:8080/api/attendance/checkin";
 
   static Future<http.Response> checkIn({
     required int employeeId,
