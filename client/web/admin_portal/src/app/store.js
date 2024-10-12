f0f22d75ae6c0afde8b2bf36ac57647f454a527e
Jeswin-J <jeswinjoseph164@gmail.com>
2024-10-12 15:15:32 +0530

    created loader

--- client/web/admin_portal/src/app/store.js
+++ client/web/admin_portal/src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import loaderReducer from './loaderSlice';
 
 const store = configureStore({
   reducer: {
-    // Add your reducers here
+    loader: loaderReducer,
   },
 });
 
