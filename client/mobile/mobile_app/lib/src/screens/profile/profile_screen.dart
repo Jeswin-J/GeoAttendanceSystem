db60ace41bab357dc46c858a4a439fdce2f7fa87
Jeswin-J <jeswinjoseph164@gmail.com>
2024-10-13 14:53:31 +0530

    created screens

--- client/mobile/mobile_app/lib/src/screens/profile/profile_screen.dart
+++ client/mobile/mobile_app/lib/src/screens/profile/profile_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
-class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({super.key});
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
