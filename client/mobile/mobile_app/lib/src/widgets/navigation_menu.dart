4691d4d672d260a09c695a46c71dee9a012934d8
Jeswin-J <jeswinjoseph164@gmail.com>
2024-10-16 17:01:16 +0530

    UI changes

--- client/mobile/mobile_app/lib/src/widgets/navigation_menu.dart
+++ client/mobile/mobile_app/lib/src/widgets/navigation_menu.dart
@@ -15,26 +15,48 @@ class NavigationMenu extends StatelessWidget {
     return Scaffold(
       bottomNavigationBar: Obx(
         () => NavigationBar(
-          height: 80,
-          elevation: 0,
-          indicatorColor: Colors.blueAccent.withOpacity(0.1),
+          height: 70,
+          elevation: 4,
+
+          indicatorShape: const RoundedRectangleBorder(
+            side: BorderSide(
+                color: Colors.blue,
+                strokeAlign: BorderSide.strokeAlignOutside,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+          ),
+          indicatorColor: Colors.blue.withOpacity(0.1),
           selectedIndex: controller.selectedIndex.value,
           onDestinationSelected: (index) => controller.selectedIndex.value = index,
+          backgroundColor: Colors.white,
+          shadowColor: Colors.black,
           destinations: [
             NavigationDestination(
-              icon: const Icon(Iconsax.home),
+              icon: const Icon(
+                  Iconsax.home,
+                  size: 24,
+              ),
               label: AppLocalizations.of(context)!.nav_home,
             ),
             NavigationDestination(
-              icon: const Icon(Iconsax.receipt),
+              icon: const Icon(
+                  Iconsax.receipt,
+                  size: 24,
+              ),
               label: AppLocalizations.of(context)!.nav_attendance,
             ),
             NavigationDestination(
-              icon: const Icon(Iconsax.setting_24),
+              icon: const Icon(
+                  Iconsax.setting_24,
+                  size: 24,
+              ),
               label: AppLocalizations.of(context)!.nav_settings,
             ),
             NavigationDestination(
-              icon: const Icon(Iconsax.user),
+              icon: const Icon(
+                  Iconsax.user,
+                  size: 24,
+              ),
               label: AppLocalizations.of(context)!.nav_profile,
             ),
         ],
