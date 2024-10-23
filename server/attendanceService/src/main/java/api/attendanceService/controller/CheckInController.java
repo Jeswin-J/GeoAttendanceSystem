e71eb4dc04bb84e33d5fe98e294a70132de04fe5
Jeswin-J <jeswinjoseph164@gmail.com>
2024-10-23 19:36:59 +0530

    created checkin controller methods

--- server/attendanceService/src/main/java/api/attendanceService/controller/CheckInController.java
+++ server/attendanceService/src/main/java/api/attendanceService/controller/CheckInController.java
@@ -1,8 +1,47 @@
 package api.attendanceService.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import api.attendanceService.model.Attendance;
+import api.attendanceService.service.CheckIn;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("api/checkIn/")
 public class CheckInController {
-    
+
+    @Autowired
+    private CheckIn checkin;
+
+
+    /**
+     * Records the check-out time and geolocation for the employee.
+     */
+    @PostMapping("/")
+    public ResponseEntity<Attendance> checkIn(@RequestBody String employeeId){
+
+        return ResponseEntity.ok(null);
+    }
+
+
+    /**
+     * Retrieves the current check-in status of the specified employee (e.g., checked in or not).
+     */
+    @PostMapping("/status/{employeeId}")
+    public ResponseEntity<String> getStatus(@RequestBody String employeeId){
+
+        return ResponseEntity.ok("STATUS");
+    }
+
+
+    /**
+     * Retrieves a list of all users who checked in today.
+     */
+    @GetMapping("/today")
+    public ResponseEntity<List<Attendance>> getTodayCheckIns(){
+
+        return ResponseEntity.ok(null);
+    }
 }
--- server/attendanceService/src/main/java/api/attendanceService/controller/CheckOutController.java
+++ server/attendanceService/src/main/java/api/attendanceService/controller/CheckOutController.java
@@ -1,8 +1,10 @@
 package api.attendanceService.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/checkOut/")
 public class CheckOutController {
     
 }
