8adc04caba2b72d2a7a8211ca9b26c1f3d8ef8ec
Jeswin-J <jeswinjoseph164@gmail.com>
2024-10-30 19:30:17 +0530

    created location access entity

--- server/locationService/src/main/java/api/locationService/model/LocationAccessEntity.java
+++ server/locationService/src/main/java/api/locationService/model/LocationAccessEntity.java
@@ -1,4 +1,57 @@
 package api.locationService.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "location_access")
 public class LocationAccessEntity {
+    @Id
+    private Long locationAccessId;
+
+    @Column(nullable = false)
+    private String employeeId;
+
+    @ManyToOne
+    @Column(nullable = false)
+    @JoinColumn(name = "locationId", nullable = false)
+    private LocationEntity locationId;
+
+    @Column(nullable = false)
+    private Boolean isAccessRevoked = false;
+
+    public Long getLocationAccessId() {
+        return locationAccessId;
+    }
+
+    public LocationAccessEntity setLocationAccessId(Long locationAccessId) {
+        this.locationAccessId = locationAccessId;
+        return this;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public LocationAccessEntity setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+        return this;
+    }
+
+    public LocationEntity getLocationId() {
+        return locationId;
+    }
+
+    public LocationAccessEntity setLocationId(LocationEntity locationId) {
+        this.locationId = locationId;
+        return this;
+    }
+
+    public Boolean getAccessRevoked() {
+        return isAccessRevoked;
+    }
+
+    public LocationAccessEntity setAccessRevoked(Boolean accessRevoked) {
+        isAccessRevoked = accessRevoked;
+        return this;
+    }
 }
