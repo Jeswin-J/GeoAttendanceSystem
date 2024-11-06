2964bf890397fc0eab8ff839a688914c3188200e
Jeswin-J <jeswinjoseph164@gmail.com>
2024-11-06 20:21:55 +0530

    created edit info button in locations details page

--- client/web/admin_portal/src/components/features/MapView/MapView.jsx
+++ client/web/admin_portal/src/components/features/MapView/MapView.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Map from "../../common/Map/Map";
 import './MapView.css';
+import Button from "../../common/Button/Button";
 
 function MapView({ location }) {
 
@@ -12,9 +13,22 @@ function MapView({ location }) {
         <div className="map-view">
             <div className="map-info-grid">
                 <div className="map-container">
-                    <h4 style={{marginBottom: 0}}>Address</h4>
-                    <p>{location.address}</p>
-                    <p>Type: {location.type}</p>
+                    <div>
+                        <h4 style={{marginBottom: 0}}>Address</h4>
+                        <p>{location.address}</p>
+                        <p>Type: {location.type}</p>
+                    </div>
+
+                    <div className="center">
+                        <Button
+                            className="edit-button"
+                            onClick={() => alert('Location information has been edited.')}
+                        >
+                            <i className="bi bi-pencil-square"></i>
+                            Edit Information
+                        </Button>
+                    </div>
+
                 </div>
 
                 <div className="map-container">
