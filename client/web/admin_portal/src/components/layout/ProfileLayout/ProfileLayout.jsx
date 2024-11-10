38bd9ee4398191ad40224b8121c70ebb56f98d7d
Jeswin-J <jeswinjoseph164@gmail.com>
2024-11-10 16:01:23 +0530

    updated

--- client/web/admin_portal/src/components/layout/ProfileLayout/ProfileLayout.jsx
+++ client/web/admin_portal/src/components/layout/ProfileLayout/ProfileLayout.jsx
@@ -1,87 +1,75 @@
 import React from 'react';
-import "./ProfileLayout.css"
+import "./ProfileLayout.css";
 import Dp from "../../common/Dp/Dp";
 import dpPic from "../../../assets/images/img.png";
 import Button from "../../common/Button/Button";
 import Input from "../../common/Input/Input";
 import Select from "../../common/Select/Select";
-import {departmentOptions, designationOptions, employeeTypeOptions, statusOptions} from "../../../data/employee/data";
+import { departmentOptions, designationOptions, employeeTypeOptions, statusOptions } from "../../../data/employee/data";
+import AttendanceStatus from "../../features/AttendanceStatus/AttendanceStatus";
 
+function ProfileLayout({ props }) {
+    const {
+        profilePictureUrl,
+        firstName,
+        lastName,
+        employeeId,
+        department,
+        designation,
+        status,
+        workEmail,
+        phoneNumber,
+        employeeType
+    } = props;
 
-
-function ProfileLayout({props}) {
-    const profilePicUrl = props.profilePictureUrl || dpPic;
     return (
         <div className="grid-container">
+            {/* Profile Image Section */}
             <div className="profile-image">
-                <Dp name={`${props.firstName} ${props.lastName}`} dp={profilePicUrl} designation={props.designation} />
-                <Button name="edit-image"
-                variant="link">
+                <Dp name={`${firstName} ${lastName}`} dp={profilePictureUrl || dpPic} designation={designation} />
+                <Button name="edit-image" variant="link">
                     <i className="bi bi-pencil-square"></i>
                     Change Image
                 </Button>
             </div>
+
+            {/* Profile Information Section */}
             <div>
                 <h3>Employee Profile</h3>
-                <br/>
+                <br />
+
                 <div className="name-field-div">
-                    <Input
-                        disabled
-                        label="Employee ID"
-                        value={props.employeeId}
-                        name="employeeId"/>
-
-                    <Input
-                        disabled
-                        label="First Name"
-                        value={props.firstName}
-                        name="firstName"/>
-
-                    <Input
-                        disabled
-                        label="Last Name"
-                        value={props.lastName}
-                        name="lastName"/>
+                    <Input disabled label="Employee ID" value={employeeId} name="employeeId" />
+                    <Input disabled label="First Name" value={firstName} name="firstName" />
+                    <Input disabled label="Last Name" value={lastName} name="lastName" />
                 </div>
-                <div className="second-field-div">
-                    <Input
-                        disabled
-                        label="Official Email Address"
-                        value={props.workEmail}
-                        name="workEmail"/>
 
-                    <Select
-                        disabled
-                        label="Employment Type"
-                        value={props.employeeType}
-                        options={employeeTypeOptions}
-                        name="firstName"/>
-                </div>
                 <div className="third-field-div">
-                    <Select
-                        disabled
-                        label="Department"
-                        value={props.department}
-                        options={departmentOptions}
-                        name="department"/>
+                    <Select disabled label="Department" value={department} options={departmentOptions} name="department" />
+                    <Select disabled label="Designation" value={designation} options={designationOptions} name="designation" />
+                    <Select disabled label="Status" value={status} options={statusOptions} name="status" />
+                </div>
 
-                    <Select
-                        disabled
-                        label="Designation"
-                        value={props.designation}
-                        options={designationOptions}
-                        name="designation"/>
-
-                    <Select
-                        disabled
-                        label="Status"
-                        value={props.status}
-                        options={statusOptions}
-                        name="firstName"/>
+                <div className="name-field-div">
+                    <Input disabled label="Email Address" value={workEmail} name="workEmail" />
+                    <Input disabled label="Contact Number" value={phoneNumber} name="phoneNumber" />
+                    <Select disabled label="Employment Type" value={employeeType} options={employeeTypeOptions} name="employeeType" />
                 </div>
             </div>
+
+            {/* Employee Access Section */}
             <div>
-            <h3>Employee Access</h3>
+                <h3>Attendance Status</h3>
+                <AttendanceStatus status="present"/>
+                <h3>Reporting To</h3>
+                <br/>
+                <Input disabled value="Manager Name" name="reportingTo"/>
+                <div className="center">
+                    <Button>
+                        <i className="bi bi-person"></i>
+                        View Profile
+                    </Button>
+                </div>
             </div>
         </div>
     );
