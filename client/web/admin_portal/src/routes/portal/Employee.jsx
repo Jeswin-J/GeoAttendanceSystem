cca984ac7208251ab5421ea54b4bf599f9320a28
Jeswin-J <jeswinjoseph164@gmail.com>
2024-11-10 21:07:39 +0530

    UI update

--- client/web/admin_portal/src/routes/portal/Employee.jsx
+++ client/web/admin_portal/src/routes/portal/Employee.jsx
@@ -5,6 +5,8 @@ import Table from '../../components/common/Table/Table';
 import Portal from "../../components/layout/Portal/Portal";
 import { useNavigate } from "react-router-dom";
 import AddEmployee from "../../components/features/AddEmployee/AddEmployee";
+import IconCard from "../../components/common/IconCard/IconCard";
+import InfoCardGroup from "../../components/features/InfoCardGroup/InfoCardGroup";
 
 function Employee() {
     const dispatch = useDispatch();
@@ -31,8 +33,17 @@ function Employee() {
     if (loading) return <p>Loading employee data...</p>;
     if (error) return <p>Error: {error}</p>;
 
+    const cardData = [
+        { title: 'Total Employees', value: `10000`, icon: <i className="bi bi-people"></i> },
+        { title: 'Full-Time Employees', value: '987', icon: <i className="bi bi-watch"></i> },
+        { title: 'Gender Ratio', value: "2 : 1", icon: <i className="bi bi-percent"></i> },
+        { title: 'New Hires', value: "1000", icon: <i className="bi bi-stars"></i> },
+        { title: 'Average Tenure', value: '5 Years', icon: <i className="bi bi-person-workspace"></i> },
+    ];
+
     return (
         <Portal>
+            <InfoCardGroup cardData={cardData}/>
             <Table
                 tableHeading="Employee Directory"
                 columns={columns}
