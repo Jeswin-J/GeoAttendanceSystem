60a1a4b17107c5bb423fd116a268814b5ade4be6
Jeswin-J <jeswinjoseph164@gmail.com>
2024-11-11 11:22:37 +0530

    location page ui update

--- client/web/admin_portal/src/routes/portal/Locations.jsx
+++ client/web/admin_portal/src/routes/portal/Locations.jsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchLocations } from '../../app/locationSlice';
 import AddLocation from "../../components/features/AddLocation/AddLocation";
 import {useNavigate} from "react-router-dom";
+import InfoCardGroup from "../../components/features/InfoCardGroup/InfoCardGroup";
+import GridLayout from "../../components/layout/GridLayout/GridLayout";
 
 function Locations() {
     const dispatch = useDispatch();
@@ -20,26 +22,41 @@ function Locations() {
     const columns = [
         { key: 'locationId', label: 'ID' },
         { key: 'locationName', label: 'Location' },
-        { key: 'latitude', label: 'Latitude' },
-        { key: 'longitude', label: 'Longitude' },
+        // { key: 'latitude', label: 'Latitude' },
+        // { key: 'longitude', label: 'Longitude' },
         { key: 'radius', label: 'Geofence (m)' },
         { key: 'division', label: 'Region' },
-        { key: 'address', label: 'Address' },
+        // { key: 'address', label: 'Address' },
         { key: 'type', label: 'Type' },
     ];
 
     if (loading) return <p>Loading locations...</p>;
     if (error) return <p>Error: {error}</p>;
 
+    const cardData = [
+        { title: 'Office Locations', value: `10000`, icon: <i className="bi bi-buildings"></i> },
+        { title: 'Unique Visitors', value: '3209423', icon: <i className="bi bi-person-badge"></i> },
+        { title: 'Highest Footfall', value: 'GAIL Office, Delhi', icon: <i className="bi bi-graph-up-arrow"></i> },
+        { title: 'Least Footfall', value: "GAIL Office, Mumbai", icon: <i className="bi bi-graph-down-arrow"></i> },
+        { title: 'Avg. Dwell Time', value: '8.5 Hrs', icon: <i className="bi bi-hourglass-split"></i> },
+    ];
+
     return (
         <Portal>
-            <Table
-                tableHeading="Locations"
-                columns={columns}
-                data={locations}
-                onRowClick={(row) => navigate(`/location/${row.locationId}`)}
-            />
-            <AddLocation/>
+            <InfoCardGroup cardData={cardData} />
+            <GridLayout columns={2} columnSizes={["4fr", "3fr"]} gap={"10px"} alignItems={"center"}>
+                <div>
+                    <Table
+                        tableHeading="Locations"
+                        columns={columns}
+                        data={locations}
+                        onRowClick={(row) => navigate(`/location/${row.locationId}`)}
+                    />
+                    <br />
+                    <AddLocation/>
+                </div>
+                <div></div>
+            </GridLayout>
         </Portal>
     );
 }
